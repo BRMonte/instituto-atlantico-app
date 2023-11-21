6a89b33866af55979129552139a737723ea6c9e1
Bruno Montebello <brunormontebello@hotmail.com>
2023-11-21 13:38:11 +0000

    Reset frontend app + bootstrap

---
+++ frontend_api/src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import { HomePage } from './pages/homePage';
+import { AboutPage } from './pages/aboutPage';
+import { Container, Navbar } from 'react-bootstrap';
+function App() {
+  return (
+    <>
+      <Container>
+        <Navbar expand="lg" variant="light" bg="light">
+          <Container>
+            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/about">About Me</Link>
+          </Container>
+        </Navbar><br />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+export default App;
