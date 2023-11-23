b8cc7e1d5c87fb585167ad993b2221a2f5930b21
Bruno Montebello <brunormontebello@hotmail.com>
2023-11-23 16:06:15 +0000

    Set Movie association + create slugfy method + create average_score method

--- moviepedia/app/models/movie.rb
+++ moviepedia/app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  has_many :ratings
+
+  before_create :slugfy
+
+  def slugfy
+    self.slug = title.parametrize
+  end
+
+  def average_score
+    ratings.average(:score).round(2).to_f
+  end
 end
